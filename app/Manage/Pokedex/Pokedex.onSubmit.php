459be936786a0cb0a21e82137276abbbe116d878
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-07-27 16:55:53 +0300

    Develop Pokedex.onSubmit

--- app/Manage/Pokedex/Pokedex.onSubmit.php
+++ app/Manage/Pokedex/Pokedex.onSubmit.php
@@ -6,8 +6,17 @@
  * Time: 15:54
  */
 
-if (isset($_POST['action']))
+if (isset($_POST['action'])) {
+
+    global $DBH, $DBT;
+    include_once('app/class.dbTable.php');
+    include_once('app/dbSpec/db.tables.php');
+    $tbPokegender = new dbTable($DBH,'pokegender',$DBT['pokegender']);
+    $tbPokedex = new dbTable($DBH,'pokedex',$DBT['pokedex']);
+
     switch ($_POST['action']) {
         case 'Build':
+
             break;
     }
+}
