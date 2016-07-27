a796b05c9a20d39bd13228cab4a402aa630d1e7c
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-07-27 16:25:38 +0300

    Develop Module::Pokedex

--- app/Manage/Pokedex/Pokedex.view.php
+++ app/Manage/Pokedex/Pokedex.view.php
@@ -12,3 +12,13 @@ include_once('app/dbSpec/db.tables.php');
 $tbPokegender = new dbTable($DBH,'pokegender',$DBT['pokegender']);
 $tbPokedex = new dbTable($DBH,'pokedex',$DBT['pokedex']);
 
+$dbtbDependeciesExists = true;
+$dbtbTargetExists = true;
+if (!$tbPokegender->exists()) {
+    print alert('Required table `pokegender` doesn\'t exist. Please, complete Fetch Gender Data');
+}
+if (!$tbPokedex->exists()) {
+    $populate = true;
+    $tbPokegender->create();
+    print alert('Create TABLE pokegender','info');
+}
