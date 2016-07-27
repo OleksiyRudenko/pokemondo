969f54c90c5de08eaa5a3a74e463ffb6214ee566
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-07-27 16:51:55 +0300

    Add fn.bootstrap::button*()

--- app/fn.bootstrap.php
+++ app/fn.bootstrap.php
@@ -17,3 +17,33 @@ function varExport(&$v,$name=false) {
 function ahref($url,$view) {
     return htmlElement('a',$view,['href'=>$url]);
 }
+
+/**
+ * @param $view
+ * @param string $style : default|primary|success|info|warning|danger|link
+ * @param string $size : lg|md|sm|xs
+ * @param $attr
+ * @return string
+ */
+function button($view, $style="primary", $size="lg", $attr=[]) {
+    if (!isset($attr['type'])) $attr['type']='BUTTON';
+    $attr['class'][] = 'btn';
+    $attr['class'][] = 'btn-'.$style;
+    $attr['class'][] = 'btn-'.$size;
+    $attr['class'] = implode(' ',$attr['class']);
+    return htmlElement('button',$view,$attr);
+}
+
+/**
+ * @param string $value
+ * @param string $view
+ * @param string $style
+ * @param string $size
+ * @param array $attr
+ * @return string
+ */
+function buttonSubmit($value='submit', $view='Submit', $style="primary", $size="lg", $attr=[]) {
+    $attr['type'] = 'SUBMIT';
+    $attr['value'] = $value;
+    return button($view,$style,$size,$attr);
+}
