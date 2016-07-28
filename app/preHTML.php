e01b76e87c51eb6e202da08000b29569caca76f9
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-07-28 21:46:29 +0300

    Check _REQUEST not _POST for submission requests

--- app/preHTML.php
+++ app/preHTML.php
@@ -9,7 +9,7 @@
 // =================== User Form Submissions Processing
 if (MODULE::currSetting('onSubmit'))
     foreach (MODULE::currSetting('onSubmit') as $action)
-        if ($_POST['action']==$action) {
+        if ($_REQUEST['action']==$action) {
             MODULE::loadOnSubmit();
             break;
         }
