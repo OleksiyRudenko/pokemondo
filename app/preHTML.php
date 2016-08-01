0d8b6dd404cc51ab7245334f42de4700aac8d693
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-08-01 11:05:12 +0300

    Fix 'action' test

--- app/preHTML.php
+++ app/preHTML.php
@@ -9,7 +9,7 @@
 // =================== User Form Submissions Processing
 if (MODULE::currSetting('onSubmit'))
     foreach (MODULE::currSetting('onSubmit') as $action)
-        if ($_REQUEST['action']==$action) {
+        if (@$_REQUEST['action']==$action) {
             MODULE::loadOnSubmit();
             break;
         }
