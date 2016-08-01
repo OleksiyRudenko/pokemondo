29742f098877599c65d2e77345f332b25d2ee8a9
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-08-01 18:56:47 +0300

    Create login splash

--- app/Public/Public.view.php
+++ app/Public/Public.view.php
@@ -1,4 +1,7 @@
-<img src="img/welcome.jpg" class="img-rounded img-responsive" alt="Welcome" width="623" height="389">
+<div class="overlayContainer col-xs-12">
+    <div class="col-xs-12"><a class="loginFBbttn overlayElement" href="#" onClick="logInWithFacebook()">Войти через Facebook</a></div>
+    <img src="img/welcome.jpg" class="img-rounded img-responsive" alt="Welcome" width="623" height="389">
+</div>
 <?php
 /**
  * Created by PhpStorm.
