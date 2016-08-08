83ca04d2b86b9f8c8af4cc640cd2d3532cb9808e
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-08-08 17:32:56 +0300

    Stub class.USER::initialize()

--- app/class.USER.php
+++ app/class.USER.php
@@ -6,6 +6,23 @@
  * Time: 17:22
  */
 
+define('UMASK_GUEST',1);
+define('UMASK_REGISTERED',2);
+define('UMASK_ADMIN',4);
+define('UMASK_ROOT',128);
+
 class USER {
+    public static $u;
+
+    public static function initialize() {
+        session_start();
+        if (!isset($_SESSION['user'])) {
+            $_SESSION['user']=[];
+        }
+        self::$u = &$_SESSION['user'];
+        if (!isset(self::$u['umask']))
+            self::$u['umask'] = UMASK_GUEST; // guest
+
+    }
 
 }
