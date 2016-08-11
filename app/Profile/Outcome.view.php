77c570111c9eb2b24d8accfe90fababa92864bbc
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-08-11 21:14:48 +0300

    Complete view logic

--- app/Profile/Outcome.view.php
+++ app/Profile/Outcome.view.php
@@ -1,6 +1,4 @@
-<p class="lead">Public user profile</p>
-<p>Logged in ARGV reference owner user sees his/her own poke profile picture.</p>
-<p>Guest/logged in not ARGV reference owner user sees log in splash as in web-site root.</p>
+<!-- p class="lead">А вот какой!</p -->
 <?php
 /**
  * Created by PhpStorm.
@@ -8,3 +6,25 @@
  * Date: 10/08/2016
  * Time: 20:31
  */
+
+if (count(ARGV::$a) && ARGV::$a[0]==USER::getUrlId()) {
+    /*
+       showProfileImage()
+       showAlternatives()
+    */
+    ?>
+    <div class="col-xs-12">
+        <?=UsermonProfile::$currentProfile->profileImgTag()?>
+    </div>
+    <?php
+    //TODO:  showAlternatives()
+} else {
+    ?>
+    <div class="row">
+        <div class="overlayContainer col-xs-12">
+            <div class="col-xs-12"><a class="loginFBbttn overlayElement" href="#" onClick="logInWithFacebook()">Войти через Facebook</a></div>
+            <img src="img/welcome.jpg" class="img-rounded img-responsive" alt="Welcome" width="623" height="389">
+        </div>
+    </div>
+    <?php
+}
