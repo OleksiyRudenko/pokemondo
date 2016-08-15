28dafbd1d51a0ebe32261183dacc82fb8865a26f
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-08-15 20:12:15 +0300

    Add pokemon alternatives

--- app/Profile/Outcome.view.php
+++ app/Profile/Outcome.view.php
@@ -16,7 +16,14 @@ if (count(ARGV::$a) && ARGV::$a[0]==USER::getUrlId()) {
         <?=UsermonProfile::$currentProfile->profileImgTag()?>
     </div>
     <?php
-    //TODO:  showAlternatives()
+    foreach (UsermonProfile::$pokemonList as $poke) {
+        print '<div class="col-xs-4 text-center">'
+            .htmlElementSingle('img',
+                ['src'=>$poke->imageUrl('local','avatar','static','normal')]
+            )
+            .'<p class="lead text-center">'.$poke->p['pokename_ru'].'</p>'
+            .'</div>';
+    }
 } else {
     ?>
     <div class="row">
